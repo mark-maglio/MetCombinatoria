9540f06d5c015e1f5699e0ef93ce313bb2642241
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-31 12:59:57 +0100

    inizio es

---
+++ CalcoliCombinatori/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcoliCombinatori")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcoliCombinatori")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4098bec9-f845-4b09-9c65-f5017d5799f8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- EquazioniLibrary/Class1.cs
+++ EquazioniLibrary/Class1.cs
@@ -8,5 +8,14 @@ namespace EquazioniLibrary
 {
     public class Class1
     {
+        public static long Fattoriale(int n)
+        {
+            long fatt = 1;
+            for (int i =n;i >= 1; i--)
+            {
+                fatt = fatt * i;
+            }
+            return fatt;
+        }
     }
 }

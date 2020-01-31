106b64befad6261d75a3daaad48c6a95b5889c4d
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-31 13:27:59 +0100

    .

---
+++ CalcoliCombinatori/Calcolo_test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquazioniLibrary;
+namespace CalcoliCombinatori
+{
+    [TestClass]
+    public class Calcolo_Test
+    {
+        [TestMethod]
+        public void Fattoriale()
+        {
+            int n = 5;
+            long valore_calcolo = Calcolo.Fattoriale(n);
+            long valore_aspettato = 120;
+            Assert.AreEqual(valore_aspettato, valore_calcolo);
+        }
+        [DataTestMethod]
+        [DataRow(5,120)]
+        [DataRow(7,)]
+    }
+}
